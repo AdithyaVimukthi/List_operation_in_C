6a76ff4d2be76544841b604ab2b39ad141f04c2c
AdithyaVimukthi <85384599+AdithyaVimukthi@users.noreply.github.com>
2022-03-26 14:23:45 +0530

    final

--- list_operations/list_new.c
+++ list_operations/list_new.c
@@ -1,11 +1,14 @@
+//including libraries
 #include <stdio.h>
 #include <stdlib.h>
 
+//define the structure of a node
 struct node {
     int data;
     struct node* nlink;
 };
 
+//Define the functions
 struct node* insertdata(int getdata, struct node* hlink);
 
 void printList(struct node* hlink);
@@ -28,47 +31,48 @@ void deleteNode(struct node **head);
 
 void removeList(struct node **hlink);
 
+//main function
 int main(){
-	struct node* headp = NULL;
+	struct node* headp = NULL;  		//Start with empty list
 	
-	headp = insertdata(12, headp);
+	headp = insertdata(12, headp);		//Inserting first five nodes
 	headp = insertdata(32, headp);
 	headp = insertdata(-5, headp);
 	headp = insertdata(123, headp);
 	headp = insertdata(-23, headp);
 	
+	printList(headp); 	//Print the list
+	printf("\n");
+	
+	srhnode(headp); 	//Serching the nodes by node value
+	printf("\n");
+	
+	updatenode(headp);  //Updating the exsisting node value 
 	printList(headp);
 	printf("\n");
 	
-	srhnode(headp);
-	printf("\n");
-	
-	updatenode(headp);
+	addnode(headp);		//Adding new node as last node of the list
 	printList(headp);
 	printf("\n");
 	
-	addnode(headp);
+	comprenode(headp);	//Compere 2 node values
+	printf("\n");
+	
+	displaynode(headp);	//Display a single node
+	printf("\n");
+	
+	countlistsize(headp);//Count the size of the list (Number of nodes)
+	printf("\n");
+	
+	sortList(headp);	//Sorting the nodes according to values
 	printList(headp);
 	printf("\n");
 	
-	comprenode(headp);
-	printf("\n");
-	
-	displaynode(headp);
-	printf("\n");
-	
-	countlistsize(headp);
-	printf("\n");
-	
-	sortList(headp);
+	deleteNode(&headp);	//Deleting a single node
 	printList(headp);
 	printf("\n");
 	
-	deleteNode(&headp);
-	printList(headp);
-	printf("\n");
-	
-	removeList(&headp);
+	removeList(&headp);	//Deleting the whole list
 	printList(headp);
 	printf("\n");
 	
@@ -77,35 +81,38 @@ int main(){
 
 
 struct node* insertdata (int getdata, struct node* hlink){
-    struct node* loop = NULL;
+    struct node* loop = NULL; 
     struct node* prev = NULL;
 
+	//Alocating memory for new node
     struct node* newnode = malloc(sizeof(struct node));
 
+	//Assigning value for new node
     (*newnode).data = getdata;
     (*newnode).nlink = NULL;
 
-    if(hlink == NULL){
-        hlink = newnode;
-    }else{
+    if(hlink == NULL){				//If list is empty make the new node as first node
+        hlink = newnode; 
+    }else{							//If the list has nodes
         loop = hlink;
-        while(loop!=NULL){
+        while(loop!=NULL){			//Find the last node
             prev = loop;
             loop=(*loop).nlink;
         }
-        (*prev).nlink = newnode;
+        (*prev).nlink = newnode;	//Add new node as last node
     }
-    return hlink;
+    return hlink;					//Update the list
 }
 
-void printList(struct node* hlink){
+//function for print the list
+void printList(struct node* hlink){			
 	struct node*loop = NULL;
-	loop = hlink;
+	loop = hlink;						//Assigning head node
 	while(loop!=NULL){
-		printf("| %d |",(*loop).data);
-		printf(" %p |",(*loop).nlink);
-		loop=(*loop).nlink;
-		if(loop != NULL){
+		printf("| %d |",(*loop).data);	//Print the node data part
+		printf(" %p |",(*loop).nlink);	//Print the next node address
+		loop=(*loop).nlink;				//go to next node
+		if(loop != NULL){				//Arrow printing mechanism
 			printf(" --> ");
 		}else{
 			printf(" - End of the List -");
@@ -114,24 +121,25 @@ void printList(struct node* hlink){
 	printf("\n");
 }
 
+//Function for search node
 void srhnode(struct node* hlink){
-	int srhval,nodenum,fudornot;
+	int srhval,nodenum,fudornot;			//Creating variables
 	struct node*loop = NULL;
 	loop = hlink;
 	
-	printf("Enter a value for search: ");
+	printf("Enter a value for search: ");	//Ask searching value from user
     scanf("%d", &srhval);
     
     while(loop!=NULL){
     	nodenum ++;
-    	if(srhval == (*loop).data){
+    	if(srhval == (*loop).data){			//Check whether any node value is equal to given search value
     		fudornot = 1;
     		break;	
 		}
-		loop=(*loop).nlink;	
+		loop=(*loop).nlink;					//Go to next node
 	}
 	
-	if(fudornot == 1){
+	if(fudornot == 1){						//Publish the results of search
 		printf("Value found in: %d node ",nodenum);
 	}else{
 		printf("Value not found in list.");
@@ -139,28 +147,29 @@ void srhnode(struct node* hlink){
 	printf("\n");
 }
 
+//function for update exsisting node
 int updatenode(struct node* hlink){
 	int nodenum,upval,getnum,fudornot;
 	struct node*loop = NULL;
 	loop = hlink;
 	
-	printf("Enter updating node number: ");
+	printf("Enter updating node number: "); //Get updating node number
     scanf("%d", &getnum);
     
-    printf("Enter updating value : ");
+    printf("Enter updating value : ");		//Get updating value
     scanf("%d", &upval);
     
-    while(loop!=NULL){
+    while(loop!=NULL){		
     	nodenum ++;
-    	if(nodenum == getnum){
-    		(*loop).data = upval;
+    	if(nodenum == getnum){		
+    		(*loop).data = upval;			//Updating the node value
     		fudornot = 1;
     		break;	
 		}
 		loop=(*loop).nlink;
 	}
 	
-	if(fudornot == 1){
+	if(fudornot == 1){						//Publish updated results
 		printf("Node %d value is updated to %d ",getnum,upval);
 	}else{
 		printf("Node is not found");
@@ -169,21 +178,22 @@ int updatenode(struct node* hlink){
 	return 0;	
 }
 
+//Add node to the list
 int addnode(struct node* hlink){
 	int newval;
 	
-	printf("Enter new value : ");
+	printf("Enter new value : ");						//Ask for new value
     scanf("%d", &newval);
     
     struct node* loop = NULL;
     struct node* prev = NULL;
 
-    struct node* newnode = malloc(sizeof(struct node));
+    struct node* newnode = malloc(sizeof(struct node)); //Memory allocation for new node
 
-    (*newnode).data = newval;
+    (*newnode).data = newval;							//Creating new node and assigning value
     (*newnode).nlink = NULL;
 
-    if(hlink == NULL){
+    if(hlink == NULL){									//Adding the new node to the list
         hlink = newnode;
     }else{
         loop = hlink;
@@ -196,12 +206,13 @@ int addnode(struct node* hlink){
     return 0;
 }
 
+//Function for compering 2 node values
 void comprenode(struct node* hlink){
-	int val1, val2, node1, node2, nodenum;
+	int val1, val2, node1, node2, nodenum; //Creating variables
 	struct node*loop = NULL;
 	loop = hlink;
 	
-	printf("Enter node number 1 : ");
+	printf("Enter node number 1 : "); //Get node numbers
     scanf("%d", &node1);
     
     printf("Enter node number 2 : ");
@@ -209,14 +220,14 @@ void comprenode(struct node* hlink){
     
     while(loop!=NULL){
     	nodenum ++;
-    	if(nodenum == node1){
+    	if(nodenum == node1){			//Assign node values to variables
     		val1 = (*loop).data;
 		}else if(nodenum == node2){
 			val2 = (*loop).data;
 		}
 		loop=(*loop).nlink;	
 	}
-    if (val1 > val2){
+    if (val1 > val2){					//Compering node values and printing results
     	printf("Node %d  value is grater than node %d  value ", node1, node2);
 	}else if (val1 == val2){
 		printf("Node %d  value is equals to node %d  value ", node1, node2);
@@ -226,18 +237,19 @@ void comprenode(struct node* hlink){
 	printf("\n");
 }
 
+//Function for display single node
 void displaynode(struct node* hlink){
 	int disnode, nodeval,nodenum;
 	struct node*loop = hlink;
 	
-	printf("Enter node number to display: ");
+	printf("Enter node number to display: ");	//Get node number
     scanf("%d", &disnode);
     
     while(loop!=NULL){
     	nodenum ++;
-    	if(nodenum == disnode){
+    	if(nodenum == disnode){					//Visit for given node
     		nodeval = (*loop).data;
-    		printf("Node Vlaue is: %d",nodeval);
+    		printf("Node Vlaue is: %d",nodeval);//Print the node value
     		break;	
 		}
 		loop=(*loop).nlink;	
@@ -245,16 +257,17 @@ void displaynode(struct node* hlink){
 	printf("\n");
 }
 
+//Function for count the list size
 int countlistsize(struct node* hlink){
 	int nodenum;
 	struct node*loop = hlink;
 	
 	while(loop!=NULL){
-    	nodenum ++;
+    	nodenum ++;						//Counting the nodes
 		loop=(*loop).nlink;	
 	}
 	
-	printf("List size is: %d",nodenum);
+	printf("List size is: %d",nodenum);	//Print the results
 	printf("\n");
 	return nodenum;
 }
@@ -263,14 +276,13 @@ int sortList(struct node* hlink) {
         //Node current will point to head  
         struct node *loop = hlink, *next = NULL, *prev = NULL;  
         int temp,code;  
-          
+        
+		//Get sorting order
         printf("Sort Low to High - Enter 1 , Sort High to Low - Enter 2 : ");
     	scanf("%d", &code);
     	
-    	if (code == 1){
-    		
+    	if (code == 1){ //Sorting the list to ascending order
     		while(loop != NULL) {  
-                //Node index will point to node next to current  
             	next = (*loop).nlink;    
             	while(next != NULL) {  
                     //If current node's data is greater than index's node data, swap the data between them  
@@ -284,13 +296,11 @@ int sortList(struct node* hlink) {
             	loop = (*loop).nlink;  
         	} 
         	
-		}else if (code == 2){
-			
-			while(loop != NULL) {  
-                //Node index will point to node next to current  
+		}else if (code == 2){//Sorting the list to descending order
+			while(loop != NULL) {   
             	next = (*loop).nlink;    
             	while(next != NULL) {  
-                    //If current node's data is greater than index's node data, swap the data between them  
+                    //If current node's data is lower than index's node data, swap the data between them  
                 	if((*loop).data < (*next).data) {  
                     	temp = (*loop).data;  
                     	(*loop).data = (*next).data;  
@@ -302,66 +312,67 @@ int sortList(struct node* hlink) {
         	} 
 			
 		}else {
-			printf("Invalide index!");
+			printf("Invalide index!"); 
 		}
          
-         printf("\n");     
-        return 0;
-    }  
+    printf("\n");     
+    return 0;
+}  
 
+//Function for Delete node
 void deleteNode(struct node **hlink){
-      //temp is used to freeing the memory
       struct node *temp;
       int val;
       struct node *loop  = *hlink;
       
-      printf("Enter deleting value : ");
+      printf("Enter deleting value : "); //Get deleting value
       scanf("%d", &val);
 
-      //key found on the head node.
-      //move to head node to the next and free the head.
+      //Move to head node to the next and free the head.
       if((*(*hlink)).data == val)
       {
-          temp = *hlink;    //backup to free the memory
+          temp = *hlink;    //Backup to free the memory
           *hlink = (*(*hlink)).nlink;
           free(temp);
       }
-      else
+      else //If deleting middle node or end node
       {
           while((*loop).nlink != NULL)
           {
-              //if yes, we need to delete the current->next node
               if((*(*loop).nlink).data == val)
               {
-                  temp = loop->nlink;
-                  //node will be disconnected from the linked list.
+                  temp = (*loop).nlink;
                   (*loop).nlink =(*(*loop).nlink).nlink;
                   free(temp);
                   break;
               }
-              //Otherwise, move the current node and proceed
               else
-                  loop = (*loop).nlink;
+                  loop = (*loop).nlink;//Go to next node
           }
       }
 	  printf("\n");
 }
 
-/* Function to delete the entire linked list */
+// Function to delete the entire linked list 
 void removeList(struct node **hlink){
-   /* deref head_ref to get the real head */
+
    struct node* loop = *hlink;
    struct node* nlink;
- 
-   while (loop != NULL)
-   {
-       nlink = (*loop).nlink;
-       free(loop);
-       loop = nlink;
-   }
-   
-   /* deref head_ref to affect the real head back
-      in the caller. */
-   *hlink = NULL;
-}
+   int val;
 
+	printf("Press 1 for delete list: "); //Get confirmation for delete list
+    scanf("%d", &val);
+
+	if(val == 1){
+		while (loop != NULL)
+   		{
+       		nlink = (*loop).nlink; 		//Deleteing all nodes
+       		free(loop);
+       		loop = nlink;
+
+   		}
+   		*hlink = NULL;					//Clear the head node
+		printf("List is deleted");
+	}
+   	
+}
